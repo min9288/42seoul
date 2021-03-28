894729d71a5f32a6fff374d89cbf6911257fc00c
minujeon <wjdalsdnchlr@naver.com>
2021-03-28 09:35:37 +0900

    rush00

--- README.md
+++
@@ -1,2 +0,0 @@
-# 42seoul
-for submission
---
+++ RUSH_00/ex00/rush01.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snam <snam@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/27 14:34:08 by snam              #+#    #+#             */
+/*   Updated: 2021/03/27 14:59:46 by snam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_putchar.c"
+
+void	first_line_print(int count_x, int x)
+{
+	while (0 < count_x && count_x <= x)
+	{
+		if (count_x == 1)
+		{
+			ft_putchar('/');
+		}
+		else if (1 < count_x && count_x < x)
+		{
+			ft_putchar('*');
+		}
+		else
+		{
+			ft_putchar('\\');
+		}
+		count_x++;
+	}
+}
+
+void	middle_line_print(int count_x, int x)
+{
+	while (0 < count_x && count_x <= x)
+	{
+		if (count_x == 1)
+		{
+			ft_putchar('*');
+		}
+		else if (1 < count_x && count_x < x)
+		{
+			ft_putchar(' ');
+		}
+		else
+		{
+			ft_putchar('*');
+		}
+		count_x++;
+	}
+}
+
+void	last_line_print(int count_x, int x)
+{
+	while (0 < count_x && count_x <= x)
+	{
+		if (count_x == 1)
+		{
+			ft_putchar('\\');
+		}
+		else if (1 < count_x && count_x < x)
+		{
+			ft_putchar('*');
+		}
+		else
+		{
+			ft_putchar('/');
+		}
+		count_x++;
+	}
+}
+
+void	rush(int x, int y)
+{
+	int count_x;
+	int count_y;
+
+	count_y = 1;
+	while (0 < count_y && count_y <= y)
+	{
+		count_x = 1;
+		if (count_y == 1)
+			first_line_print(count_x, x);
+		else if (1 < count_y && count_y < y)
+			middle_line_print(count_x, x);
+		else
+			last_line_print(count_x, x);
+		ft_putchar('\n');
+		count_y++;
+	}
+}
